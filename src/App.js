19c0895e5f2f38866392e9818752147c912fc2fc
bala-veeravalli <bala.veeravalli@gmail.com>
2020-01-02 00:47:18 +0530

    header component changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Header from "./components/header/header.component";
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Header></Header>
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route path="/shop" component={ShopPage}/>
