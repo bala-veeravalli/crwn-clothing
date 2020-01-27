5cc60ba0d698768541e6b278236301240464ce96
bala-veeravalli <bala.veeravalli@gmail.com>
2020-01-27 20:05:55 +0530

    add cart items functionality with memoization with reselect

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { connect } from "react-redux";
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from "../../redux/cart/cart.actions";
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ name, price, imageUrl}) => {
+const CollectionItem = ({ item, addItem}) => {
+    const { name, price, imageUrl } = item;
     return (
         <div className="collection-item">
             <div
@@ -14,8 +18,13 @@ const CollectionItem = ({ name, price, imageUrl}) => {
                 <span className="name">{ name }</span>
                 <span className="price">{ price }</span>
             </div>
+            <CustomButton inverted onClick={() => addItem(item)}>ADD TO CART</CustomButton>
         </div>
     )
 }
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: (item) => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
