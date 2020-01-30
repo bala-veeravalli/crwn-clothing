cdbe9c2ac1d684a6f373332b8e7c8d6c2cbb9aef
bala-veeravalli <bala.veeravalli@gmail.com>
2020-01-30 09:24:11 +0530

    moved our directory and shop data into individual reducers and created and updated respective selectors and components

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import * as serviceWorker from './serviceWorker';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
